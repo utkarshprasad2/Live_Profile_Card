885782ff255354431380f44b5e90fe2ab24f2722
utkarsh prasad <uprasad3@illinois.edu>
2025-06-08 23:51:26 -0700

    Update Next.js config and layout

--- next.config.js
+++ next.config.js
@@ -1,25 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { isServer }) => {
-    // Handle .lynx files
-    config.module.rules.push({
-      test: /\.lynx$/,
-      use: [
-        {
-          loader: '@lynx/webpack-loader',
-          options: {
-            isServer,
-          },
-        },
-      ],
-    });
-
-    return config;
-  },
-  // Enable experimental features for Lynx integration
-  experimental: {
-    appDir: true,
-    serverComponents: true,
+  images: {
+    domains: ['p16-sign-va.tiktokcdn.com', 'p16-sign-sg.tiktokcdn.com'],
   },
 }
 
